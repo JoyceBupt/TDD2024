d40c9df309997301f00cca15e8d58afde02dc1b3
JoyceBupt <JoyceBupt@gmail.com>
2024-03-21 14:27:00 +0800

    feat: 使用unittest模块扩展了功能测试

--- functinal_tests.py
+++ functinal_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    
+        def setUp(self):
+            self.browser = webdriver.Chrome()
+            # self.browser.implicitly_wait(3)
+    
+        def tearDown(self):
+            self.browser.quit()
+    
+        def test_can_start_a_list_and_retrieve_it_later(self):
+            # Edith has heard about a cool new online to-do app. She goes
+            # to check out its homepage
+            self.browser.get('http://localhost:8000')
+    
+            # She notices the page title and header mention to-do lists
+            self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
+            self.fail('Finish the test!')
 
-assert 'Django' in browser.page_source
+if __name__ == '__main__':
+    # unittest.main(warnings='ignore')
+    unittest.main()
