91238cc99f4a452904d7545a1a45b802b143b212
JoyceBupt <JoyceBupt@gmail.com>
2024-03-21 17:05:06 +0800

    message 5: The first iterative development

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,22 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page  # (1)
+from lists.views import home_page
+from django.http import HttpRequest
 
 
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')  # (2)
-        self.assertEqual(found.func, home_page)  # (3)
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-# Create your tests here.
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+        # self.assertIn('<h1>To-Do lists</h1>', html)
+        # self.assertTrue(html.strip().endswith('</html>'))
+        # self.assertTemplateUsed(response, 'home.html')
