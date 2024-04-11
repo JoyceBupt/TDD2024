9adf0fe0a883152eb2965ed5eb176a263d2baade
JoyceBupt <JoyceBupt@gmail.com>
2024-04-11 14:58:26 +0800

    message 9: After refactoring the functional_tests

--- functinal_tests.py
+++ functinal_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -32,6 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy flowers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy flowers')
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了"Give a gift to Lisi"
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
@@ -39,10 +45,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
 
         self.fail('Finish the test!')
 
